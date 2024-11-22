df7d56fcfdf7b16fd3c65a749b562fadda68f713
maplesift <a23050716@gmail.com>
2024-11-22 13:54:03 +0800

    物件導向:繼承

--- index.php
+++ index.php
@@ -14,9 +14,10 @@
 
     class Animal{
         // 宣告公開
-        public $type='animal';
-        public $name='John';
-        public $hair_color='black';
+        protected $type='animal';
+        protected $name='John';
+        protected $hair_color='black';
+        protected $feet=['front-left','front-right','back-left','back-right'];
     
      function __construct($type,$name,$hair_color){
         $this->type=$type;
@@ -29,20 +30,46 @@
     
     public function speed(){
         echo $this->name.'is running at 20km/h';
+
+    }
+    public function getName(){
+        return $this->name;
+    }
+    public function setName($name){
+        $this->name=$name;
     }
 }
     // 實例化 (instance)
     $cat=new Animal('cat','kitty','white');
 
-    echo $cat->type;
-    echo $cat->name;
-    echo $cat->hair_color;
+    // echo $cat->type;
+    echo $cat->getName();
+    // echo $cat->hair_color;
     echo $cat->run();
     echo $cat->speed();
+    // print_r($cat->feet);
 
+    $cat->setName('Joson');
+    echo $cat->getName();
+    ?>
+    <h1>繼承</h1>
+    <?php
+    class Cat extends Animal{
+        protected $type='cat';
+        protected $name='Judy';
+        function __construct($hair_color){
+            $this->hair_color=$hair_color; 
+        }
 
+    }
+    $mycat=new Cat('white');
 
-    
+    echo $mycat->getName();
+    echo "<br>";
+    echo $mycat->run();
+    $mycat->setName('judy');
+    echo $mycat->getName();
+
     ?>
 </body>
 </html>
